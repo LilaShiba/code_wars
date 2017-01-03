32b356a76aa419f700a2f10e4fed0c20e3bd5e81
James <kjames@lfny-ltm0774.home>
2017-01-02 23:59:32 -0500

    better

--- find_cap.py
+++ find_cap.py
@@ -5,3 +5,9 @@ def capitals(word):
       if word[i].isupper():
           up.append(i)
   return up
+
+
+
+  ### Much Better and use to teach enumerate + lambda function
+
+  capitals = lambda word: [i for i,val in enumerate(word) if val.isupper()]
